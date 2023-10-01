4349310b943523436f7b44146b8c640d7901e74d
Billy Ponto <bpgponto@gmail.com>
2023-10-01 19:15:49 +0800

    setup

---
+++ assets/scripts/tailwind-config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      'primary': "'Kumbh Sans', sans-serif"
+    },
+    extend: {
+      colors: {
+        primary: '#F09638',
+        secondary: '#FEFAF6',
+        'dark-1': '#4A556C',
+        'dark-2': '#333461',
+        choco: '#85490A',
+        cream: '#F7C897',
+      }
+    }
+  }
+}
